fc691e7e9a10bd438df73512db530fbf35de8197
Leonardo Rodrigues de Oliveira <85530968+oliverlleo@users.noreply.github.com>
2026-08-13 13:32:44 -0300

    feat: load team overload guard

--- src/pages/career/career-loader-v3.js
+++ src/pages/career/career-loader-v3.js
@@ -61,3 +61,4 @@ await import('./career-avatar-sync.js?v=20260811-12');
 await import('./career-development-loop.js?v=20260813-1');
 await import('./career-ui-usability-v6.js?v=20260813-1');
 await import('./career-preparation-ui-v7.js?v=20260813-1');
+await import('./career-preparation-team-guard-v7.js?v=20260813-1');
